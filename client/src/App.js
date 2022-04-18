586e2b7d5c8bef466e3a195761e3c8e1937edc9a
Bill Sasi <billsasi@gmail.com>
2022-04-17 20:35:16 -0700

    updates to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Overview from './components/Overview';
 import TransactionHistory from './components/TransactionHistory';
 import InputForm from './components/InputForm';
 import './App.css';
 
 const App = () => {
+  const [transactions, setTransactions] = useState([]);
+
   return (
     <div className="container">
-      <h1>App</h1>
       <Overview />
       <InputForm />
       <TransactionHistory />
--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const InputForm = () => {
-  return <div>InputForm</div>;
+  const [addItem, setAddItem] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput('');
+  };
+
+  return (
+    <div className="input-form">
+      <button onClick={() => setAddItem(!addItem)}>Add Item</button>
+      {addItem && (
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Enter transaction"
+            value={input}
+            onChange={handleChange}
+          ></input>
+          <button type="submit">+</button>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default InputForm;
