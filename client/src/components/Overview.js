989f136e00112716c5c6535a178f9a1d4bc688e3
Bill Sasi <billsasi@gmail.com>
2022-04-23 16:29:59 -0700

    added savings slider

--- client/src/components/Overview.js
+++ client/src/components/Overview.js
@@ -1,24 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 const Overview = ({ transactions }) => {
   const [income, setIncome] = useState(0);
+  const [available, setAvailable] = useState(income);
 
   const getBal = () => {
     let sum = 0;
-    transactions.forEach((item) => (sum += item.amt));
+    transactions.forEach((item) => (sum += item.amount));
     return sum;
   };
 
+  let percent = 0;
+  function valuetext(value) {
+    percent = value;
+  }
+
   const net = income - getBal();
 
+  useEffect(() => {
+    setAvailable((income - 0.01 * percent * income).toFixed(2));
+  }, [income, percent]);
+
   return (
-    <div className="overview">
-      <h1 className={net > -1 ? 'positive' : 'negative'}>Balance ${net}</h1>
-      <input
-        placeholder="Income"
-        onChange={(e) => setIncome(e.target.value)}
-      ></input>
-    </div>
+    <>
+      <div className="overview">
+        <div className="balance-info">
+          <h2>Available Income: ${available}</h2>
+          <h1 className={net > -1 ? 'positive' : 'negative'}>
+            Balance ${net.toFixed(2)}
+          </h1>
+        </div>
+        <div>
+          <span>Monthly Income: </span>
+          <input
+            placeholder="Income"
+            onChange={(e) => setIncome(e.target.value)}
+          ></input>
+        </div>
+      </div>
+      <Box className="savings-slider" sx={{ width: 400 }}>
+        <h3>Enter what % you want to save:</h3>
+        <Slider
+          aria-label="Savings Percent"
+          defaultValue={20}
+          getAriaValueText={valuetext}
+          valueLabelDisplay="auto"
+          step={10}
+          marks
+          min={0}
+          max={90}
+        />
+      </Box>
+    </>
   );
 };
 
--- client/src/components/TransactionHistory.js
+++ client/src/components/TransactionHistory.js
@@ -1,22 +1,40 @@
-import Transaction from "./Transaction";
+import Transaction from './Transaction';
 
-const TransactionHistory = ({ transactions, handleDeleteTransaction, handleedit}) => {
+const TransactionHistory = ({
+  transactions,
+  handleDeleteTransaction,
+  handleedit,
+}) => {
+  console.log(transactions.length);
+
   return (
-    <>
     <div className="transactTable">
-      {transactions.length === 0 && <div className="row">No transactions</div>}
+      {transactions.length === 0 && <div>No transactions</div>}
       <table>
-      {transactions.length !== 0 && <thead><tr>
-        <th>Description</th>
-        <th>Amount</th>
-        <th></th><th></th></tr></thead>}
-      <tbody>
-      {transactions.map((item, index) => (<Transaction key={index} 
-        transact={item} handleDelete={handleDeleteTransaction} handleEdit={handleedit}/>))}
-      </tbody>
+        {transactions.length !== 0 && (
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+        )}
+        <tbody>
+          {transactions.map((item, key) => {
+            return (
+              <Transaction
+                key={key}
+                transact={item}
+                handleDelete={handleDeleteTransaction}
+                handleEdit={handleedit}
+              />
+            );
+          })}
+        </tbody>
       </table>
     </div>
-    </>
   );
 };
 
